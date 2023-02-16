fae17edc6ff02c1f68940ac7f5c7df047b56e932
ChiZhang <zliming@outlook.com>
2023-02-16 09:45:00 +0800

    add clientId

--- .tina/config.ts
+++ .tina/config.ts
@@ -5,8 +5,8 @@ const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
 
 export default defineConfig({
   branch,
-  clientId: "", // Get this from tina.io
-  token: "", // Get this from tina.io
+  clientId: "6bf9c685-d15f-437f-b9ba-6501415761d0", // Get this from tina.io
+  token: "35b633d31d52ceca38f8ac761439c36615c848a0", // Get this from tina.io
   build: {
     outputFolder: "admin",
     publicFolder: "static",
